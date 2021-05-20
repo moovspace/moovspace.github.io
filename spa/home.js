38784a8945eea189d76d970d32295434a853a76f
Moov Space <moovspace@gmail.com>
2021-05-20 15:58:08 +0200

    Add files via upload

--- spa/home.js
+++ spa/home.js
@@ -9,16 +9,13 @@ export class Page extends Component
 	{
 		// Page title
 		document.title = 'Spa javascript router (ES6)';
-
 		// Document events: click, dblclick, change, keydown, contextmenu, auxclick, mouseover ...
 		let e1 = Event.addDocument("#content", (item,index) => { console.log("Clicked! ", item, index); }, "click");
 		let e2 = Event.addDocument("#content", (item,index) => { console.log("Clicked right mouse! ", item, index); }, "auxclick");
-
 		// Window events: hashchange, load, popstate - after local link click
 		let e3 = Event.addWindow(() => {
 			console.log("Page onload event !!!")
 		}, 'load');
-
 		// Page
 		return { 'html': View.Html(div), 'document_events': [e1,e2], 'window_events': [e3] }
 	}
