2d6eb51444f6770edc8565240d4795a284569878
Moov Space <moovspace@gmail.com>
2021-05-20 16:19:40 +0200

    Homepage controller

--- spa/home.js
+++ spa/home.js
@@ -8,14 +8,17 @@ export class Page extends Component
 	Setup(div)
 	{
 		// Page title
-		document.title = 'Spa javascript router (ES6)';
+		document.title = 'Spa js app for github pages';
+
 		// Document events: click, dblclick, change, keydown, contextmenu, auxclick, mouseover ...
 		let e1 = Event.addDocument("#content", (item,index) => { console.log("Clicked! ", item, index); }, "click");
 		let e2 = Event.addDocument("#content", (item,index) => { console.log("Clicked right mouse! ", item, index); }, "auxclick");
+
 		// Window events: hashchange, load, popstate - after local link click
 		let e3 = Event.addWindow(() => {
 			console.log("Page onload event !!!")
 		}, 'load');
+
 		// Page
 		return { 'html': View.Html(div), 'document_events': [e1,e2], 'window_events': [e3] }
 	}
