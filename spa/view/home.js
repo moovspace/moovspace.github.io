87c307a69dd231c736e075ceedad853b46447aff
Moov Space <moovspace@gmail.com>
2021-05-20 15:53:57 +0200

    Add files via upload

--- spa/view/home.js
+++ spa/view/home.js
@@ -4,7 +4,11 @@ export default class HomeView
 {
 	static Home(div)
 	{
-		return `<div id="content"> Homepage content goes here (` + div + `).</div>`;
+		return `<div id="content">
+			<h1> SPA js router </h1>
+			<p> Js SPA router, single page application routing system in javascript. </p>
+			<a href="github.com/moovspace/spa-js-router"> Github repository </a>
+		</div>`;
 	}
 
 	static Html(div)
