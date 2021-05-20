5cca1ce2b68dd626e6a300e86920bdc91fbb429b
Moov Space <moovspace@gmail.com>
2021-05-20 16:11:07 +0200

    Homepage view

--- spa/view/home.js
+++ spa/view/home.js
@@ -7,7 +7,7 @@ export default class HomeView
 		return `<div id="content">
 			<h1> Spa js router </h1>
 			<p> Js spa router, single page application routing system in javascript. </p>
-			<a href="https://github.com/moovspace/spa-js-router"> Github repository </a>
+			<a href="https://github.com/moovspace/spa-js-router" title="Spa js router"> Github repository </a>
 		</div>`;
 	}
 
